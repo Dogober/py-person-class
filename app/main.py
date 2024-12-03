8e8608d6e68aa31cdb22aa73d2bf5e4d9eab74e9
git_practic <dogoberrr@gmail.com>
2024-12-03 13:08:43 +0200

    'access_to_people_through_Person_class__person_list_with_comprehension__checking_persons_params_through_get'

--- app/main.py
+++ app/main.py
@@ -4,23 +4,20 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people.update({
+        Person.people.update({
             name: self
         })
 
 
 def create_person_list(people: list) -> list:
 
-    persons = []
+    persons = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        persons.append(Person(person["name"], person["age"]))
-
-    for person in people:
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife") and person["wife"] is not None:
             Person.people[person["name"]].wife = (
                 Person.people)[person["wife"]]
-        if "husband" in person and person["husband"] is not None:
+        if person.get("husband") and person["husband"] is not None:
             Person.people[person["name"]].husband = (
                 Person.people)[person["husband"]]
 
